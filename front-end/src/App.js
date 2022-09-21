21bc88d9a67b1b69f719b45081177b0b575f124f
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-21 15:38:29 -0300

    :sparkles: feat(Frontend): define routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import Checkout from './pages/Checkout';
+import Login from './pages/Login';
+import Manage from './pages/Manage';
+import OrderDetails from './pages/OrderDetails';
+import Orders from './pages/Orders';
+import Products from './pages/Products';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Routes>
+      <Route path="/login" element={ <Login /> } />
+      <Route path="/register" element={ <Register /> } />
+      <Route path="/admin/manage" element={ <Manage /> } />
+      <Route path="/customer/products" element={ <Products /> } />
+      <Route path="/customer/checkout" element={ <Checkout /> } />
+      <Route path="/customer/orders" element={ <Orders /> } />
+      <Route path="/customer/orders/:id" element={ <OrderDetails /> } />
+      {/* É o mesmo componente da tela de pedidos do cliente, diferenciar por meio de props */}
+      <Route path="/seller/orders" element={ <Orders /> } />
+      <Route path="/seller/orders/:id" element={ <OrderDetails /> } />
+    </Routes>
   );
 }
 
---
+++ front-end/src/pages/Manage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Manage() {
+  return (
+    <div>
+      Manage
+    </div>
+  );
+}
+
+export default Manage;
