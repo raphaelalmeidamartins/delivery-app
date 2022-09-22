92126b68a37f2c6241289077ce4c8e03020380ce
romulo-rp29 <romulo.ceuma@gmail.com>
2022-09-22 19:01:40 -0300

    refactor(services): :recycle: userService: fix source branch formatting

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,7 +1,5 @@
 const { User } = require('../database/models');
 
-const create = async (data) => {
-  return User.create(data);
-};
+const create = async (data) => User.create(data);
 
 module.exports = { create };
