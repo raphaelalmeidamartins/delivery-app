ea60160ccba534d608a4142a30c799ef3c00c468
romulo-rp29 <romulo.ceuma@gmail.com>
2022-09-22 17:37:31 -0300

    feat(routes): :building_construction: updated index

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const loginRouter = require('./login.routes');
+const saleRouter = require('./saleRoutes');
 
 const router = Router();
 
 router.use('/login', loginRouter);
+router.use('/sale', saleRouter);
 
 module.exports = router;
