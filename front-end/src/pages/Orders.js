7ad45338de848d97672e2358ae26e43a8873316c
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-22 15:44:41 -0300

    :sparkles: feat(Frontend): add placeholder OrderCard

--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import OrderCard from '../components/OrderCard';
 
 function Orders() {
   return (
     <div>
       <NavBar />
-      Orders
+      <main>
+        {/* Quando o back-end estiver pronto, dar map nos pedidos retornados pela API */}
+        <OrderCard
+          id={ 1 }
+          testId={ 1 }
+          status="pending"
+          date="22/09/2022"
+          totalPrice={ 10.22 }
+        />
+      </main>
     </div>
   );
 }
