b012b1fc3c05983b0f572f5e9f20a719b4b4dd8a
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-22 17:24:37 -0300

    :sparkles: feat(Frontend): add OrderDetailsList with placeholder data

--- front-end/src/pages/OrderDetails.js
+++ front-end/src/pages/OrderDetails.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
+import OrderDetailsList from '../components/OrderDetailsList';
 
 function OrderDetails() {
+  const placeholderItems = [
+    {
+      id: 1,
+      name: 'Placeholder1',
+      quantity: 2,
+      price: 15.50,
+    },
+    {
+      id: 2,
+      name: 'Placeholder2',
+      quantity: 3,
+      price: 25.50,
+    },
+  ];
+
   return (
     <div>
-      OrderDetails
+      <NavBar />
+      <main>
+        <OrderDetailsList orderItems={ placeholderItems } editable />
+      </main>
     </div>
   );
 }
