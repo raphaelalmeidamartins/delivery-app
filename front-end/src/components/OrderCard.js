33469e635786dfb2c03ab15586d62d825688e35b
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-22 15:44:11 -0300

    :sparkles: feat(Frontend): create OrderCard component

--- front-end/src/components/OrderCard.js
+++ front-end/src/components/OrderCard.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import { AppContext } from '../context';
 import OrderStatus from './OrderStatus';
 
-function OrderCard() {
+function OrderCard({ id, status, date, totalPrice, testId }) {
+  const { userData } = useContext(AppContext);
+
   return (
-    <div>
-      OrderCard
-      <OrderStatus />
-    </div>
+    <section style={ { border: '1px solid black' } }>
+      <span data-testid={ `${userData.role}_orders__element-order-id-${testId}` }>
+        {id}
+      </span>
+      <OrderStatus status={ status } testId={ testId } />
+      <div>
+        <span
+          data-testid={ `${userData.role}_orders__element-order-date-${testId}` }
+        >
+          {date}
+        </span>
+        <span
+          data-testid={ `${userData.role}_orders__element-card-price-${testId}` }
+        >
+          {`R$ ${totalPrice.toFixed(2)}`}
+        </span>
+      </div>
+    </section>
   );
 }
+
+OrderCard.propTypes = {
+  testId: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
+  status: PropTypes.oneOf(['pending', 'preparing', 'delivered']).isRequired,
+  totalPrice: PropTypes.number.isRequired,
+  date: PropTypes.string.isRequired,
+};
 
 export default OrderCard;
