ded8f490e63fd03a65dd983c10c924b5d24c10b4
Helder Medeiros <hmedeiros211@gmail.com>
2022-09-22 18:06:32 -0300

    feat(service): :sparkles: implementing token functions

--- back-end/src/services/loginService.js
+++ back-end/src/services/loginService.js
@@ -1,7 +1,16 @@
 const { User } = require('../database/models');
+const { unauthorizedError } = require('../errors/errors');
+const { createToken } = require('../utils/jwtAuthentication');
 
-const create = async (data) => {
-  return User.create(data);
+const login = async (data) => {
+  const { email, password } = data;
+  const user = await User.findOne({ where: { email } });
+  if (!user) unauthorizedError('Email or password invalid!');
+  const passwordIsTrue = password === user.password;
+  if (!passwordIsTrue) unauthorizedError('Email or password invalid!');
+  
+  const token = createToken({ email, role: user.role, name: user.name });
+  return { email, role: user.role, name: user.name, token };
 };
 
-module.exports = { create };
+module.exports = { login };
