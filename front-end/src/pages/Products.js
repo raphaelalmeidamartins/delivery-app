4db8113b03fca8faed123e38f4ef2462eb82e7ed
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-22 15:45:09 -0300

    :sparkles: feat(Frontend): add placeholder ProductCard

--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import ProductCard from '../components/ProductCard';
 
 function Products() {
   return (
-    <main>
+    <div>
       <NavBar />
-      Products
-    </main>
+      <main>
+        {/* Quando o back-end estiver pronto, dar map nos produtos retornados pela API */}
+        <ProductCard
+          price={ 10 }
+          thumbnail="http://placekitten.com/200/300"
+          name="Placeholder"
+          testId={ 1 }
+        />
+      </main>
+    </div>
   );
 }
 
