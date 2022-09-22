e8531553c588e3252e3b4b485dd60aaba1212f84
Helder Medeiros <hmedeiros211@gmail.com>
2022-09-22 18:04:45 -0300

    feat(errorMiddleware): :sparkles: created the configuration for errors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,14 @@
+require('express-async-errors');
 const express = require('express');
+const errorHandler = require('../middlewares/errorHandler');
 const router = require('../routes');
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
-app.use(router)
+app.use(router);
+app.use(errorHandler);
 
 module.exports = app;
