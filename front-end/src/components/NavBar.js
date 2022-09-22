043aac71ffb3e77eb657a279ea6823b0687de75f
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-22 15:41:24 -0300

    :recycle: refactor(Frontend): add header tag to the NavBar

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -13,42 +13,44 @@ function NavBar() {
   };
 
   return (
-    <nav>
-      <ul>
-        {userData?.role === 'customer' && (
-          <>
-            <li data-testid="customer_products__element-navbar-link-products">
-              <NavLink to="/customer/products">PRODUTOS</NavLink>
-            </li>
+    <header style={ { border: '1px solid black' } }>
+      <nav>
+        <ul>
+          {userData?.role === 'customer' && (
+            <>
+              <li data-testid="customer_products__element-navbar-link-products">
+                <NavLink to="/customer/products">PRODUTOS</NavLink>
+              </li>
+              <li data-testid="customer_products__element-navbar-link-orders">
+                <NavLink to="/customer/orders">MEUS PEDIDOS</NavLink>
+              </li>
+            </>
+          )}
+          {userData?.role === 'seller' && (
             <li data-testid="customer_products__element-navbar-link-orders">
-              <NavLink to="/customer/orders">MEUS PEDIDOS</NavLink>
+              <NavLink to="seller/orders">PEDIDOS</NavLink>
             </li>
-          </>
-        )}
-        {userData?.role === 'seller' && (
-          <li data-testid="customer_products__element-navbar-link-orders">
-            <NavLink to="seller/orders">PEDIDOS</NavLink>
-          </li>
-        )}
-        {userData?.role === 'admin' && (
-          <li data-testid="customer_products__element-navbar-link-orders">
-            GERENCIAR USUÁRIOS
-          </li>
-        )}
-      </ul>
-      <section>
-        <span data-testid="customer_products__element-navbar-user-full-name">
-          {userData?.username}
-        </span>
-        <button
-          data-testid="customer_products__element-navbar-link-logout"
-          type="button"
-          onClick={ handleLogout }
-        >
-          Sair
-        </button>
-      </section>
-    </nav>
+          )}
+          {userData?.role === 'admin' && (
+            <li data-testid="customer_products__element-navbar-link-orders">
+              GERENCIAR USUÁRIOS
+            </li>
+          )}
+        </ul>
+        <section>
+          <span data-testid="customer_products__element-navbar-user-full-name">
+            {userData?.username}
+          </span>
+          <button
+            data-testid="customer_products__element-navbar-link-logout"
+            type="button"
+            onClick={ handleLogout }
+          >
+            Sair
+          </button>
+        </section>
+      </nav>
+    </header>
   );
 }
 
