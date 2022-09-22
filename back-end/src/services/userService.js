1f93784ef7caf6c534535056203707a36f8acc61
Arthur Teixeira <tuzinf5@gmail.com>
2022-09-22 17:14:20 -0300

    feat(services): :sparkles: creating findAll function in service layer

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -2,4 +2,6 @@ const { User } = require('../database/models');
 
 const create = async (data) => User.create(data);
 
-module.exports = { create };
+const findAll = async () => User.findAll();
+
+module.exports = { create, findAll };
