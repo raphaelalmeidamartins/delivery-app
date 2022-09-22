83fdf6d6fdbdaf9af934b678395678492173ea1a
Helder Medeiros <hmedeiros211@gmail.com>
2022-09-22 18:14:14 -0300

    style(lint): :rotating_light: fix lint errors

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -1,8 +1,8 @@
-const userService = require('../services/userService')
+const userService = require('../services/userService');
 
 const create = async (req, res) => {
   const userCreated = await userService.create(req.body);
   return res.status(201).json(userCreated);
-}
+};
 
 module.exports = { create };
