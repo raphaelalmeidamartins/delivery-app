ab5871e5f1f24587a1f235aa37fba946e203c83f
Helder Medeiros <hmedeiros211@gmail.com>
2022-09-23 19:11:18 -0300

    refactor(controllers): :recycle: function create salesProducts

--- back-end/src/controllers/saleController.js
+++ back-end/src/controllers/saleController.js
@@ -1,32 +1,39 @@
-const SaleService = require('../services/saleService');
+const saleService = require('../services/saleService');
+const productService = require('../services/productService');
 
 const create = async (req, res) => {
-  await SaleService.create(req.body);
-  res.status(201);
+  const sale = await saleService.create(req.body);
+  res.status(201).json(sale);
   };
 
-const list = async (req, res) => {
-  const sales = await SaleService.list();
+const list = async (_req, res) => {
+  const sales = await saleService.list();
   res.status(200).json(sales);
   };
 
-const findById = async (req, res) => {
+const find = async (req, res) => {
   const { id } = req.params;
-  const sale = await SaleService.findById(id);
+  const sale = await saleService.find(id);
   res.status(200).json(sale);
   };
 
 const update = async (req, res) => {
   const { id } = req.params;
-  await SaleService.update(id, req.body);
+  await saleService.update(id, req.body);
   res.status(200).send({ message: 'Sale updated!' });
   };
 
 const deleteSale = async (req, res) => {
   const { id } = req.params;
-  await SaleService.delete(id);
+  await saleService.delete(id);
   res.status(200).send({ message: 'Sale deleted!' });
   };
+
+const findByArrayId = async (_req, res) => {
+    const result = await productService.totalPrice()
+    return res.status(201).json(result);
+  };
+
 
 module.exports = {
   create,
@@ -34,4 +41,5 @@ module.exports = {
   findById,
   update,
   deleteSale,
+  findByArrayId,
 };
