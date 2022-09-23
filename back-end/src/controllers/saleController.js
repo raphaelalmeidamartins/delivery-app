3fa79baad6f94a2642223ebf5d27342a85e4172a
romulo-rp29 <romulo.ceuma@gmail.com>
2022-09-23 00:15:04 -0300

    refactor(controllers): :recycle: updated sale controller

--- back-end/src/controllers/saleController.js
+++ back-end/src/controllers/saleController.js
@@ -1,13 +1,17 @@
 const saleService = require('../services/saleService');
 
-const create = async (req, res) => {
-  await saleService.create(req.body);
-  res.status(201);
-};
+const saleController = {
 
-const list = async (_req, res) => {
+create: async (req, res) => {
+  await saleService.create(req.body);
+  res.status(201).send({ message: 'Sale created!' });
+},
+
+list: async (_req, res) => {
   const sales = await saleService.list();
   res.status(200).json(sales);
+},
+
 };
 
-module.exports = { create, list };
+module.exports = saleController;
