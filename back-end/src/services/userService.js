21602dcf97e961c66ecad3dead3afc5d8794cf33
Arthur Teixeira <tuzinf5@gmail.com>
2022-09-23 16:35:29 -0300

    refactor(services): :recycle: refactoring create function

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,6 +1,31 @@
 const { User } = require('../database/models');
+const { unauthorizedError } = require('../errors/errors');
+const { generateEncryptedPassword } = require('../utils/generateEncryptedPassword');
+const { verifyToken } = require('../utils/jwtAuthentication');
 
-const create = async (data) => User.create(data);
+const create = async (data, headers) => {
+  const { authorization } = headers;
+
+  const encryptedPassword = generateEncryptedPassword(data.password);
+
+  const userToBeCreated = { ...data, password: encryptedPassword };
+
+  const tokenIsValid = verifyToken(authorization);
+
+  if (tokenIsValid.role !== 'admin') {
+    unauthorizedError(
+      'Current user does not have the permissions to perform this request',
+    );
+  }
+
+  if (data.role !== 'customer' && tokenIsValid.role === 'admin') {
+    const userCreated = User.create(userToBeCreated);
+    return userCreated;
+  }
+
+  const userCreated = User.create(userToBeCreated);
+  return userCreated;
+};
 
 const findAll = async () => User.findAll();
 
