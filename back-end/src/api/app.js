d01a689d96ea791717035509dff0f70a362681a2
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-23 17:49:13 -0300

    :bug: fix(Backend): missing cors middleware

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,13 @@
 require('express-async-errors');
 const express = require('express');
+const cors = require('cors');
 const errorHandler = require('../middlewares/errorHandler');
 const router = require('../routes');
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
