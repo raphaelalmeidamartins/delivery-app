1cfef43f610709995f5c63f483ea46071204dab4
Arthur Teixeira <tuzinf5@gmail.com>
2022-09-23 16:34:00 -0300

    refactor(controllers): :recycle: passing parameter headers

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -1,7 +1,7 @@
 const userService = require('../services/userService');
 
 const create = async (req, res) => {
-  const userCreated = await userService.create(req.body);
+  const userCreated = await userService.create(req.body, req.headers);
   return res.status(201).json(userCreated);
 };
 
