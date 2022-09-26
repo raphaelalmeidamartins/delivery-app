a93c7a13c217b95cffd0861313e30203d551c503
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-26 16:08:34 -0300

    :sparkles: feat(Frontend): add checkout form

--- front-end/src/pages/Checkout.js
+++ front-end/src/pages/Checkout.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import React, { useState } from 'react';
 import NavBar from '../components/NavBar';
 import OrderDetailsList from '../components/OrderDetailsList';
 
@@ -18,20 +23,84 @@ function Checkout() {
     },
   ];
 
+  const [seller, setSeller] = useState('');
+  const [address, setAddress] = useState('');
+  const [addressNumber, setAddressNumber] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    const salesValues = {
+      seller: () => setSeller(value),
+      address: () => setAddress(value),
+      addressNumber: () => setAddressNumber(value),
+    };
+    salesValues[name]();
+  };
+
+  const handleSubmit = () => {};
+
   return (
-    <div>
+    <>
       <NavBar />
       <main>
         <section>
-          <h2>Finalizar pedido</h2>
-          <OrderDetailsList orderItems={ placeholderItems } />
+          <Typography component="h2" variant="h2" gutterBottom>
+            Finalizar pedido
+          </Typography>
+          <OrderDetailsList orderItems={ placeholderItems } editable />
         </section>
+        <Box component="form" onSubmit={ handleSubmit } />
         <form>
-          <h2>Detalhes e Endereço para entrega</h2>
-          {/* inserir formulário de entrega aqui */}
+          <Typography component="h2" variant="h2" gutterBottom>
+            Detalhes e Endereço para entrega
+          </Typography>
+          <FormControl>
+            <TextField
+              variant="filled"
+              label="P. Vendedora Responsável"
+              required
+              type="text"
+              name="seller"
+              value={ seller }
+              onChange={ handleChange }
+              inputProps={ { 'data-testid': 'customer_checkout__select-seller' } }
+            />
+          </FormControl>
+          <FormControl>
+            <TextField
+              variant="filled"
+              label="Endereço"
+              required
+              type="text"
+              name="address"
+              value={ address }
+              onChange={ handleChange }
+              inputProps={ { 'data-testid': 'customer_checkout__input-address' } }
+            />
+          </FormControl>
+          <FormControl>
+            <TextField
+              variant="filled"
+              label="Número"
+              required
+              type="text"
+              name="addressNumber"
+              value={ addressNumber }
+              onChange={ handleChange }
+              inputProps={ { 'data-testid': 'customer_checkout__input-address-number' } }
+            />
+          </FormControl>
+          <Button
+            component="button"
+            type="button"
+            variant="outlined"
+            data-testid="customer_checkout__button-submit-order"
+            onClick={ handleSubmit }
+          >
+            FINALIZAR PEDIDO
+          </Button>
         </form>
       </main>
-    </div>
+    </>
   );
 }
 
