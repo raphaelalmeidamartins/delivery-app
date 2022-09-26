3788563cec412b1fbaee154da70e2d210e063833
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-26 08:28:21 -0300

    :fire: remove unecessary files

--- back-end/src/errors/errors.js
+++
@@ -1,8 +0,0 @@
-const unauthorizedError = (message) => {
-  const error = new Error(message);
-  error.name = 'UnauthorizedError';
-  error.status = 401;
-  throw error;
-};
-
-module.exports = { unauthorizedError };
