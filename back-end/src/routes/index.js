d1313b0a456f131a85f6c075721998d91dc50cf7
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-26 08:30:12 -0300

    :sparkles: feat(Backend): add POST /customers endpoint

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const loginRouter = require('./login.routes');
 const userRouter = require('./users.routes');
+const customerRoutes = require('./customer.routes');
 
 const router = Router();
 
 router.use('/login', loginRouter);
-
+router.use('/customers', customerRoutes);
 router.use('/users', userRouter);
 
 module.exports = router;
