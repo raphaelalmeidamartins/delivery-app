b3208f141900bb624b057bed1f66e35ca66b0760
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-26 16:07:31 -0300

    :sparkles: feat(Frontend): add delete button

--- front-end/src/components/OrderDetailsList.js
+++ front-end/src/components/OrderDetailsList.js
@@ -4,29 +4,36 @@ import OrderDetailsListItem from './OrderDetailsListItem';
 
 function OrderDetailsList({ editable, orderItems }) {
   return (
-    <table style={ { border: '1px solid black' } }>
-      <thead>
-        <tr>
-          <th>Item</th>
-          <th>Descrição</th>
-          <th>Quantidade</th>
-          <th>Valor Unitário</th>
-          <th>Sub-total</th>
-          {editable && <th>Remover item</th>}
-        </tr>
-      </thead>
-      <tbody>
-        {/* Fazer um map nos itens do pedido */}
-        {orderItems?.map((item, index) => (
-          <OrderDetailsListItem
-            key={ item.id }
-            index={ index }
-            { ...item }
-            editable={ editable }
-          />
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <table style={ { border: '1px solid black' } }>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Sub-total</th>
+            {editable && <th>Remover item</th>}
+          </tr>
+        </thead>
+        <tbody>
+          {/* Fazer um map nos itens do pedido */}
+          {orderItems?.map((item, index) => (
+            <OrderDetailsListItem
+              key={ item.id }
+              index={ index }
+              { ...item }
+              editable={ editable }
+            />
+          ))}
+        </tbody>
+      </table>
+      <span data-testid="customer_checkout__element-order-total-price">
+        {`R$ ${orderItems
+          ?.reduce((acc, { price, quantity }) => acc + price * quantity, 0)
+          .toFixed(2)}`}
+      </span>
+    </div>
   );
 }
 
