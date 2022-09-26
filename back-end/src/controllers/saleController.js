051f859be49dc9b6031550626eba20c750d3370f
Helder Medeiros <hmedeiros211@gmail.com>
2022-09-26 16:28:19 -0300

    refactor(controller): :recycle: change to http status code

--- back-end/src/controllers/saleController.js
+++ back-end/src/controllers/saleController.js
@@ -1,31 +1,32 @@
+const { StatusCodes } = require('http-status-codes');
 const saleService = require('../services/saleService');
 
 const create = async (req, res) => {
   const sale = await saleService.create(req.body, req.headers.authorization);
-  res.status(201).json(sale);
+  res.status(StatusCodes.CREATED).json({ saleId: sale.id });
   };
 
 const list = async (_req, res) => {
   const sales = await saleService.list();
-  res.status(200).json(sales);
+  res.status(StatusCodes.OK).json(sales);
   };
 
 const find = async (req, res) => {
   const { id } = req.params;
   const sale = await saleService.find(id);
-  res.status(200).json(sale);
+  res.status(StatusCodes.OK).json(sale);
   };
 
 const update = async (req, res) => {
   const { id } = req.params;
   await saleService.update(id, req.body);
-  res.status(200).send({ message: 'Sale updated!' });
+  res.status(StatusCodes.OK).send({ message: 'Sale updated!' });
   };
 
 const deleteSale = async (req, res) => {
   const { id } = req.params;
   await saleService.delete(id);
-  res.status(200).send({ message: 'Sale deleted!' });
+  res.status(StatusCodes.OK).send({ message: 'Sale deleted!' });
   };
 
 module.exports = {
