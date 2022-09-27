3fc9479dfeb8dcf40ec08e4c518278bd310bf842
Helder Medeiros <hmedeiros211@gmail.com>
2022-09-27 18:10:41 -0300

    fix(service): :bug: change secret key

--- back-end/src/services/tokenService.js
+++ back-end/src/services/tokenService.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 const BadRequestError = require('../utils/errors/BadRequestError');
 
-const create = async (data) => jwt.sign(data, 'secret', { expiresIn: '6h' });
+const create = async (data) => jwt.sign(data, 'secret_key', { expiresIn: '6h' });
 
 const validate = (token) => {
   if (!token) {
     throw new BadRequestError('Token not found');
   }
-  return jwt.verify(token, 'secret');
+  return jwt.verify(token, 'secret_key');
 };
 
 module.exports = { create, validate };
