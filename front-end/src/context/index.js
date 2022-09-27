60d4ebe6e53605fbdfb5b93ac696c0ffce362254
Arthur Teixeira <tuzinf5@gmail.com>
2022-09-27 12:41:38 -0300

    revert: :recycle: Returning the file to the previous state

--- front-end/src/context/index.js
+++ front-end/src/context/index.js
@@ -3,47 +3,34 @@ import React, { createContext, useLayoutEffect, useMemo, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const AppContext = createContext({});
-
 function AppProvider({ children }) {
   const userDataStored = window.localStorage.getItem('userData');
   const [userData, setUserData] = useState(JSON.parse(userDataStored) || {});
-  const [productsData, setProductsData] = useState([]);
-  const contextValue = useMemo(() => (
-    {
-      userData,
-      setUserData,
-      productsData,
-      setProductsData,
-    }), [userData, productsData]);
+  const contextValue = useMemo(() => ({ userData, setUserData }), [userData]);
   const { pathname } = useLocation();
 
   const navigate = useNavigate();
 
   useLayoutEffect(() => {
-    if (!Object.keys(userData).length || !Object.keys(productsData).length) {
+    if (!Object.keys(userData).length) {
       window.localStorage.removeItem('userData');
       if (!['/login', '/register'].includes(pathname)) {
         navigate('/login');
       }
+    } else {
       window.localStorage.setItem('userData', JSON.stringify(userData));
     }
-    if (!Object.keys(productsData).length) {
-      window.localStorage.removeItem('productsData');
-      navigate('/customer/products');
-    }
-  }, [userData, productsData, pathname, navigate]);
+  }, [userData, pathname, navigate]);
 
   return (
     <AppContext.Provider value={ contextValue }>{children}</AppContext.Provider>
   );
 }
-
 AppProvider.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
   ]).isRequired,
 };
-
 export default AppProvider;
 export { AppContext };
