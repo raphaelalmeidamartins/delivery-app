4ae83eba4801408e617cbb88d959cb36ed25f0e9
Helder Medeiros <hmedeiros211@gmail.com>
2022-09-27 18:48:17 -0300

    perf(controller): :zap: Insert validate find

--- back-end/src/controllers/saleController.js
+++ back-end/src/controllers/saleController.js
@@ -18,11 +18,12 @@ const listBySeller = async (req, res) => {
 
 const find = async (req, res) => {
   const { id } = req.params;
-  const sale = await saleService.find(id);
+  const sale = await saleService.find(id, req.headers.authorization);
   res.status(StatusCodes.OK).json(sale);
   };
 
 const update = async (req, res) => {
+  saleService.validate.body(req.body);
   const { id } = req.params;
   await saleService.update(id, req.body, req.headers.authorization);
   res.sendStatus(StatusCodes.NO_CONTENT);
