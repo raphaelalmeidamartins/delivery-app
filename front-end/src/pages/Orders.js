115771018799bcc0aca6906815be3fae03cc75e7
Helder Medeiros <hmedeiros211@gmail.com>
2022-09-28 15:59:19 -0300

    feat(pages): :sparkles: creating orders page

--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -3,18 +3,44 @@ import NavBar from '../components/NavBar';
 import OrderCard from '../components/OrderCard';
 
 function Orders() {
+  const errMsg = '';
+  const data = [
+    {
+      id: 1,
+      totalPrice: '2.20',
+      saleDate: '2022-09-22T00:01:00.000Z',
+      status: 'Em Trânsito',
+    },
+    {
+      id: 2,
+      totalPrice: '7.50',
+      saleDate: '2022-09-22T00:02:00.000Z',
+      status: 'Entregue',
+    },
+    {
+      id: 3,
+      totalPrice: '2.49',
+      saleDate: '2022-09-22T00:03:00.000Z',
+      status: 'Entregue',
+    },
+  ];
   return (
     <div>
       <NavBar />
       <main>
         {/* Quando o back-end estiver pronto, dar map nos pedidos retornados pela API */}
-        <OrderCard
-          id={ 1 }
-          testId={ 1 }
-          status="pending"
-          date="22/09/2022"
-          totalPrice={ 10.22 }
-        />
+        {!!errMsg && <p>{errMsg}</p>}
+        {!errMsg
+          && data.map(({ id, totalPrice, saleDate, status }) => (
+            <OrderCard
+              key={ id }
+              id={ id }
+              testId={ id }
+              status={ status }
+              date={ saleDate }
+              totalPrice={ Number(totalPrice) }
+            />
+          ))}
       </main>
     </div>
   );
