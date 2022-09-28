a7c4e2c744b01b7e396d52c4de00c386a870a5d7
Sophia Contesini <sophiamarques214@gmail.com>
2022-09-28 19:25:27 -0300

    feat: implements OrderDetails page

--- front-end/src/pages/OrderDetails.js
+++ front-end/src/pages/OrderDetails.js
@@ -1,28 +1,78 @@
-import React from 'react';
+import { StatusCodes } from 'http-status-codes';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import OrderDetailsList from '../components/OrderDetailsList';
+import { AppContext } from '../context';
+import service from '../service';
 
 function OrderDetails() {
-  const placeholderItems = [
-    {
-      id: 1,
-      name: 'Placeholder1',
-      quantity: 2,
-      price: 15.50,
-    },
-    {
-      id: 2,
-      name: 'Placeholder2',
-      quantity: 3,
-      price: 25.50,
-    },
-  ];
+  const { userData } = useContext(AppContext);
+  const { id } = useParams();
+  const [sale, setSale] = useState({});
+  const [errMsg, setErrMsg] = useState('');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await service.get.bySaleId(userData.token, id);
+
+      const { status } = response;
+      const data = await response.json();
+      const products = data.products
+        .map(({ id: saleId, name, price, SalesProducts: { quantity } }) => ({
+          id: saleId, name, price: +price, quantity,
+        }));
+
+      if (status === StatusCodes.OK) {
+        setErrMsg('');
+        setSale(data);
+        setItems(products);
+      } else {
+        setErrMsg(data.message);
+      }
+    };
+
+    fetchData();
+  }, [userData.token, id]);
 
   return (
     <div>
       <NavBar />
       <main>
-        <OrderDetailsList orderItems={ placeholderItems } editable />
+        {!!errMsg && <p>{errMsg}</p>}
+        {!errMsg && !!Object.keys(sale).length && (
+          <>
+            <section>
+              <h2>Detalhe do Pedido</h2>
+              <div>
+                <span>
+                  PEDIDO
+                  {' '}
+                  {id}
+                </span>
+                <span>
+                  P. Vendedora
+                  {' '}
+                  {sale.seller.name }
+                </span>
+                <span>
+                  {' '}
+                  {sale.date }
+                </span>
+                <span>
+                  {' '}
+                  {sale.status }
+                </span>
+                <button type="submit">MARCAR COMO ENTREGUE</button>
+              </div>
+            </section>
+            <OrderDetailsList
+              orderItems={ items }
+              editable
+            />
+          </>)}
+
       </main>
     </div>
   );
