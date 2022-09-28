69ad57f0ec0597c9a311f549b31be1722afed9d6
Helder Medeiros <hmedeiros211@gmail.com>
2022-09-28 19:39:25 -0300

    refactor(components): :recycle: change orderCard information

--- front-end/src/components/OrderCard.js
+++ front-end/src/components/OrderCard.js
@@ -6,22 +6,26 @@ import OrderStatus from './OrderStatus';
 function OrderCard({ id, status, date, totalPrice, testId }) {
   const { userData } = useContext(AppContext);
   const FOUR_NEGATIVE = -4;
+  const TWO_NEGATIVE = -2;
+
   return (
     <section style={ { border: '1px solid black' } }>
       <span data-testid={ `${userData.role}_orders__element-order-id-${testId}` }>
-        {`Pedido ${(`0000${id}`).slice(FOUR_NEGATIVE)}`}
+        {`Pedido ${`0000${id}`.slice(FOUR_NEGATIVE)}`}
       </span>
       <OrderStatus status={ status } testId={ testId } />
       <div>
         <span
           data-testid={ `${userData.role}_orders__element-order-date-${testId}` }
         >
-          {`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`}
+          {`${date.getDate()}/${`00${date.getMonth() + 1}`.slice(
+            TWO_NEGATIVE,
+          )}/${date.getFullYear()}`}
         </span>
         <span
           data-testid={ `${userData.role}_orders__element-card-price-${testId}` }
         >
-          {`R$ ${totalPrice.toFixed(2)}`}
+          {`${totalPrice.toFixed(2).replace('.', ',')}`}
         </span>
       </div>
     </section>
