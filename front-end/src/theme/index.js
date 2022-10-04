591d89bcc62ca33d3f342917de60972e45e3f116
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-04 15:00:12 -0300

    :lipstick: style(Froentend): create MUI theme

--- front-end/src/theme/index.js
+++ front-end/src/theme/index.js
@@ -1,5 +1,31 @@
 import { createTheme } from '@mui/material';
 
-const theme = createTheme({});
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#bf360c',
+    },
+    secondary: {
+      main: '#3e2723',
+      contrastText: '#fce4ec',
+    },
+    background: {
+      default: '#fce4ec',
+      login: '#fbc4ab',
+    },
+    productCard: {
+      main: '#CB5E3C',
+      contrastText: '#3e2723',
+    },
+    chip: {
+      main: '#efe9ae',
+      contrastText: '#3e2723',
+    },
+  },
+  typography: {
+    fontFamily: '"\'Inter Tight\'", "Helvetica", "Arial", sans-serif',
+  },
+});
 
 export default theme;
