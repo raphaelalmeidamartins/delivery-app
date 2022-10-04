198d7354d83193cfcbd191182e808c57a58d3fe8
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-04 01:50:31 -0300

    :bug: fix(Froentend): missing nav icon

--- front-end/src/components/NavBarItem.js
+++ front-end/src/components/NavBarItem.js
@@ -1,9 +1,29 @@
-import { MenuItem } from '@mui/material';
+import { MenuItem, SvgIcon } from '@mui/material';
 import Button from '@mui/material/Button';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { FaBeer } from 'react-icons/fa';
+import { RiShoppingBagFill } from 'react-icons/ri';
 
 function NavBarItem({ name, path, testId, onClick, button }) {
+  const icons = {
+    Produtos: (
+      <SvgIcon sx={ { marginRight: '8px' } }>
+        <FaBeer />
+      </SvgIcon>
+    ),
+    'Meus Pedidos': (
+      <SvgIcon sx={ { marginRight: '8px' } }>
+        <RiShoppingBagFill />
+      </SvgIcon>
+    ),
+    Pedidos: (
+      <SvgIcon sx={ { marginRight: '8px' } }>
+        <RiShoppingBagFill />
+      </SvgIcon>
+    ),
+  };
+
   return (
     <div>
       {button && (
@@ -15,11 +35,13 @@ function NavBarItem({ name, path, testId, onClick, button }) {
           onClick={ () => onClick(path) }
           sx={ { color: 'white' } }
         >
+          {icons[name]}
           {name}
         </Button>
       )}
       {!button && (
         <MenuItem onClick={ () => onClick(path) }>
+          {icons[name]}
           <Button variant="text" type="button">
             {name}
           </Button>
