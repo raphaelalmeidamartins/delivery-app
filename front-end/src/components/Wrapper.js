2030de297a6b08a39e32f67411412ad69fb11543
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-04 14:58:59 -0300

    :sparkles: feat(Frontend): add props to customize style

--- front-end/src/components/Wrapper.js
+++ front-end/src/components/Wrapper.js
@@ -1,27 +1,48 @@
+import { useTheme } from '@emotion/react';
+import Box from '@mui/material/Box';
 import Container from '@mui/system/Container';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function Wrapper({ children }) {
+function Wrapper({ children, sx, bgSx }) {
+  const { palette } = useTheme();
+
   return (
-    <Container
-      component="main"
+    <Box
       sx={ {
-        paddingTop: { xs: '100px', md: '140px' },
-        paddingBottom: '70px',
-        position: 'relative',
+        width: '100vw',
+        minHeight: '100vh',
+        backgroundColor: palette.background.default,
+        ...bgSx,
       } }
     >
-      {children}
-    </Container>
+      <Container
+        component="main"
+        sx={ {
+          paddingTop: { xs: '100px', md: '140px' },
+          paddingBottom: '70px',
+          position: 'relative',
+          ...sx,
+        } }
+      >
+        {children}
+      </Container>
+    </Box>
   );
 }
+
+Wrapper.defaultProps = {
+  sx: {},
+  bgSx: {},
+};
 
 Wrapper.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
   ]).isRequired,
+  sx: PropTypes.objectOf(PropTypes.string),
+  bgSx: PropTypes.objectOf(PropTypes.string),
 };
 
 export default Wrapper;
